dc22217e9ffd02cb2c8982a635152997253f16d9
Prakash KS <Prakash KS@INC00387-.Incture.local>
2019-07-10 13:54:27 +0530

    adding departmnet

---
+++ sample-common-module/sample-entities/src/main/java/org/sample/entities/Deparrment.java
@@ -0,0 +1,23 @@
+package org.sample.entities;
+
+public class Deparrment {
+	private Integer deptId;
+	private Integer deptName;
+
+	public Integer getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+
+	public Integer getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(Integer deptName) {
+		this.deptName = deptName;
+	}
+
+}
--- sample-common-module/sample-entities/src/main/java/org/sample/entities/Employee.java
+++ sample-common-module/sample-entities/src/main/java/org/sample/entities/Employee.java
@@ -7,6 +7,7 @@ public class Employee {
 	private Integer empId;
 	private String empName;
 	private BigDecimal salary;
+	private String role;
 
 	public Integer getEmpId() {
 		return empId;
@@ -30,6 +31,14 @@ public class Employee {
 
 	public void setSalary(BigDecimal salary) {
 		this.salary = salary;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
 	}
 
 }
