d0eb9e8b303aa6133251924e6ace9b8216266f1f
Prakash KS <Prakash KS@INC00387-.Incture.local>
2019-07-10 12:35:04 +0530

    commiting new file

---
+++ sample-common-module/sample-entities/src/main/java/org/sample/entities/Employee.java
@@ -0,0 +1,35 @@
+package org.sample.entities;
+
+import java.math.BigDecimal;
+
+public class Employee {
+
+	private Integer empId;
+	private String empName;
+	private BigDecimal salary;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public BigDecimal getSalary() {
+		return salary;
+	}
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+
+}
